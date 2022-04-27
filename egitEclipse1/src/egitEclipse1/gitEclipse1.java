8792c0bb54e50781de27fc43da2ebeac5922c9a2
pavan <pavanrm86@gmail.com>
2022-04-27 20:00:39 +0530

    homepage reviews resolution

--- egitEclipse1/src/egitEclipse1/gitEclipse1.java
+++ egitEclipse1/src/egitEclipse1/gitEclipse1.java
@@ -5,7 +5,8 @@ public class gitEclipse1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           System.out.println("hello world");
-         System.out.println("homepage branch");
+          System.out.println("hello1");
+         System.out.println("homepage");
          System.out.println("homepage branch park");
 	}
 
